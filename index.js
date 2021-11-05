4c77b1c2718f7e6e87e319ccc077a1cb37d7773c
Jordan <jstrande@rams.colostate.edu>
2021-11-04 19:41:38 -0600

    10.1 10/16 database, /movies

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { response } = require('express');
 const express = require('express');
 
+const db = require("./dbConnectExec.js");
+
+
 const app = express();
 
 app.listen(5000,()=>{console.log("app is running on port 5000")});
@@ -8,3 +11,17 @@ app.listen(5000,()=>{console.log("app is running on port 5000")});
 app.get("/hi",(req, res)=>{res.send("Hello World")});
 
 app.get("/",(req, res)=>{res.send("Api is running")});
+
+app.get("/movies", (req, res)=>
+{
+    //get data from database
+    db.executeQuery(`SELECT *
+    FROM movies 
+    LEFT JOIN Genre
+    ON genre.GenrePK = movie.GenreFK`).then((theResults)=>{
+        res.status(200).send(theResults);
+    }).catch((myError)=>{
+        console.log(myError);
+        res.status(500).send();
+    })
+})
